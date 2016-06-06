fa35b4388a6fb7528d87b3bf7d031dd84a9d3d4f
Josh Bertsche and Ryan Carlile <ryancarlile@gmail.com>
2016-06-06 15:33:58 -0700

    initial commit

---
+++ app/routes/index.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(args) {
+    return this.store.findAll(args);
+  },
+
+  actions: {
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
